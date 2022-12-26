8c4ab05970d99052c6542bfc1aeb804265c70eef
initdc <initd@outlook.com>
2022-12-26 02:48:22 +0000

    build: add base by `bundle gem libconsole`

---
+++ spec/libconsole_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Libconsole do
+  it "has a version number" do
+    expect(Libconsole::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
