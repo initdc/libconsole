4b4700ab22abb0239c1972c69fdd6092fca201c1
initdc <initd@outlook.com>
2022-12-29 06:56:03 +0000

    refactor: rewrite `Libconsole::Color`
    Support short func name

--- lib/libconsole/color.rb
+++ lib/libconsole/color.rb
@@ -6,35 +6,82 @@ module Libconsole
 
     # rubocop:disable Layout/HashAlignment
 
-    # https://github.com/rubyworks/ansi/blob/master/lib/ansi/bbcode.rb
-    ## ANSIname => ANSIcode LUT
-    ANSINAME2CODE = {
-      "reset"     => "\e[0m",    "bold"       => "\e[1m",
-      "dim"       => "\e[2m",    "italic"     => "\e[3m",
-      "underline" => "\e[4m",    "blink"      => "\e[5m",
-      "fastblink" => "\e[6m",    "reverse"    => "\e[7m",
-      "hide"      => "\e[8m",    "strike"     => "\e[9m",
-      "black"     => "\e[0;30m", "darkgrey"   => "\e[1;30m",
-      "red"       => "\e[0;31m", "lightred"   => "\e[1;31m",
-      "green"     => "\e[0;32m", "lightgreen" => "\e[1;32m",
-      "brown"     => "\e[0;33m", "yellow"     => "\e[1;33m",
-      "blue"      => "\e[0;34m", "lightblue"  => "\e[1;34m",
-      "purple"    => "\e[0;35m", "magenta"    => "\e[1;35m",
-      "cyan"      => "\e[0;36m", "lightcyan"  => "\e[1;36m",
-      "grey"      => "\e[0;37m", "white"      => "\e[1;37m",
-      "bgblack"   => "\e[40m",   "bgred"      => "\e[41m",
-      "bggreen"   => "\e[42m",   "bgyellow"   => "\e[43m",
-      "bgblue"    => "\e[44m",   "bgmagenta"  => "\e[45m",
-      "bgcyan"    => "\e[46m",   "bgwhite"    => "\e[47m"
-    }.freeze
-    # rubocop:enable Layout/HashAlignment
+    # https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+    Escape = {
+      uni:  "\u001b",
+      oct:  "\033",
+      hex:  "\x1B",
+      lang: "\e"
+    }
 
-    ANSINAME2CODE.each do |name, code|
-      define_method(name) do |any = nil|
-        if any
-          %(#{code}#{any}\e[0m)
-        else
-          code
+    # https://gist.github.com/iamnewton/8754917
+    Format = {
+      reset:     [0, "E"],  bold:    [1, "B"],
+      dim:       [2, "D"],  italic:  [3, "I"],
+      underline: [4, "U"],  blink:   [5, "N"],
+      fastblink: [6, "F"],  reverse: [7, "V"],
+      hide:      [8, "H"],  strike:  [9, "S"]
+    }
+
+    # https://ss64.com/nt/syntax-ansi.html
+    Color = {
+      black:     [0, "K"],  red:     [1, "R"],
+      green:     [2, "G"],  yellow:  [3, "Y"],
+      blue:      [4, "L"],  magenta: [5, "M"],
+      cyan:      [6, "C"],  white:   [7, "W"]
+    }
+
+    Range = {
+      ""        => 30,
+      "bg"      => 40,
+      "light"   => 90,
+      "bglight" => 100
+    }
+
+    def _no_empty(argv)
+      argv.any? ? argv : nil
+    end
+
+    Escape.each do |std, pre|
+      Format.each do |name, pair|
+        code = pair[0]
+        alias_name = pair[1]
+
+        fn_name = "#{std}_#{name}"
+        define_method(fn_name) do |any = nil|
+          if any
+            "#{pre}[#{code}m#{any}#{pre}[0m"
+          else
+            "#{pre}[#{code}m"
+          end
+        end
+
+        alias_method alias_name, fn_name
+        alias_method name, fn_name if std == :oct
+      end
+
+      Range.each do |n_pre, c_base|
+        Color.each do |n_base, pair|
+          c_add = pair[0]
+          alias_name = pair[1]
+
+          name = "#{n_pre}#{n_base}"
+          code = c_base + c_add
+          fn_name = "#{std}_#{name}"
+          define_method(fn_name) do |any = nil|
+            if any
+              "#{pre}[#{code}m#{any}#{pre}[0m"
+            else
+              "#{pre}[#{code}m"
+            end
+          end
+
+          alias_method alias_name, fn_name if n_pre == ""
+          alias_method "G#{alias_name}", fn_name if n_pre == "bg"
+          alias_method "L#{alias_name}", fn_name if n_pre == "light"
+          alias_method "GL#{alias_name}", fn_name if n_pre == "bglight"
+
+          alias_method name, fn_name if std == :oct
         end
       end
     end
